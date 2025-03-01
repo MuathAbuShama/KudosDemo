5f2cb82feffec273315693ff36a6d29ecbbea022
MuathAbuShama <81920267+MuathAbuShama@users.noreply.github.com>
2025-03-02 02:17:10 +0300

    Add files via upload

---
+++ assets/images/logo/jquery-flipster-template/jquery-flipster-template/README.md
@@ -0,0 +1,5 @@
+# jQuery Flipster Template
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/shshaw/pen/oxQowz](https://codepen.io/shshaw/pen/oxQowz).
+
+https://github.com/drien/jquery-flipster/
---
+++ assets/images/logo/jquery-flipster-template/jquery-flipster-template/dist/script.js
@@ -0,0 +1,17 @@
+console.clear();
+
+var flipContainer = $('.flipster'),
+    flipItemContainer = flipContainer.find('.flip-items'),
+    flipItem = flipContainer.find('li');
+
+flipContainer.flipster({
+  itemContainer: flipItemContainer,
+  itemSelector: flipItem,
+  loop: true,
+  autoplay: 1000,
+  style: 'carousel',
+  spacing: -0.5,
+  scrollwheel: false,
+  //nav: 'after',
+  buttons: false
+});
